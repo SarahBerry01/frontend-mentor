6af8e58e764a2b1b0c8c22912d3346750bd13d9e
SarahBerry01 <sarah@berrys.me.uk>
2023-08-08 21:00:56 +0100

    Add colors and seperate files

--- time-tracking-dashboard/src/Breakdown.js
+++ time-tracking-dashboard/src/Breakdown.js
@@ -1,14 +1,38 @@
 import './Breakdown.css';
-function Breakdown(){
+function Breakdown() {
     return (
         <div className="breakdownGrid">
-            <div className="breakdownCard">1<div className="statsCard">a</div></div>
-            <div className="breakdownCard">2<div className="statsCard">a</div></div>
-            <div className="breakdownCard">3<div className="statsCard">a</div></div>
-            <div className="breakdownCard">4<div className="statsCard">a</div></div>
-            <div className="breakdownCard">5<div className="statsCard">a</div></div>
-            <div className="breakdownCard">6<div className="statsCard">a</div></div>
+            <div className="breakdownCard">1
+                <div className="statsCard">
+                    <p className="time">1hr</p>
+                </div>
+            </div>
+            <div className="breakdownCard">2
+                <div className="statsCard">
+                    <p className="time">1hr</p>
+                </div>
+            </div>
+            <div className="breakdownCard">3
+                <div className="statsCard">
+                    <p className="time">1hr</p>
+                </div>
+            </div>
+            <div className="breakdownCard">4
+                <div className="statsCard">
+                    <p className="time">1hr</p>
+                </div>
+            </div>
+            <div className="breakdownCard">5
+                <div className="statsCard">
+                    <p className="time">1hr</p>
+                </div>
+            </div>
+            <div className="breakdownCard">6
+                <div className="statsCard">
+                    <p className="time">1hr</p>
+                </div>
+            </div>
         </div>
     )
-    }
+}
 export default Breakdown;
