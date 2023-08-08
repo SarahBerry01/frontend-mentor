2ea225a8298672b05a277c84f59f9d2631fd58a2
SarahBerry01 <sarah@berrys.me.uk>
2023-08-08 20:12:08 +0100

    add initial grid layout

--- time-tracking-dashboard/src/App.js
+++ time-tracking-dashboard/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Breakdown from './Breakdown';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="body">
+        <div className="userMenu">
+          <div className="userCard">userCard </div>
+          Usermenu
+        </div>
+        <Breakdown/>
+      </div>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+        Coded by Sarah Berry.
+      </div>
+    </div >
   );
 }
 
